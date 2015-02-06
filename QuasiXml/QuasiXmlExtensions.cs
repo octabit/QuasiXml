1e481325384712556ed127201ab575dc45dc725b
octabit_cp <SND\octabit_cp>
2015-02-06 22:38:36 +0000

    Changed verion to 0.9.0

---
+++ QuasiXml/QuasiXmlExtensions.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright © 2015 Kevin Thomasson, thomassonkevin@gmail.com
+ *
+ * This file is part of QuasiXml.
+ *
+ * QuasiXml is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QuasiXml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QuasiXml.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * License: GNU Lesser General Public License (LGPL)
+ * Source code: https://quasixml.codeplex.com/
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QuasiXml
+{
+    public static class QuasiXmlExtensions
+    {
+        public static int IndexOf<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate, int startIndex)
+        {
+            for (int i = startIndex; i < source.Count(); i++)
+            {
+                TSource c = source.ElementAt(i);
+                if (predicate(c))
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
