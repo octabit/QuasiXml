243c12c198652c7afc56487c87e91b83870bd338
octabit_cp <SND\octabit_cp>
2015-02-13 23:00:16 +0000

    Branched from $/quasixml/main

---
+++ 0.9.2b/QuasiXmlTest/QuasiXmlParserTests.cs
@@ -0,0 +1,301 @@
+﻿/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright © 2015 Kevin Thomasson, thomassonkevin@gmail.com
+ *
+ * This file is part of QuasiXml.
+ *
+ * QuasiXml is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QuasiXml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QuasiXml.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * License: GNU Lesser General Public License (LGPL)
+ * Source code: https://quasixml.codeplex.com/
+ */
+using System;
+using System.Xml;
+using QuasiXml;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuasiXmlTest
+{
+    [TestClass]
+    public class QuasiXmlParserTests
+    {
+        [TestMethod]
+        public void TestCanParseElement()
+        {
+            string markup =
+            @"<root>
+                <element>content <subelement>text</subelement></element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual(2, root["element"].Children.Count);
+            Assert.AreEqual("element", root.Children[0].Name);
+            Assert.AreEqual("content text", root["element"].InnerText);
+        }
+
+        [TestMethod]
+        public void TestCanParseAttribute()
+        {
+            string markup =
+            @"<root>
+                <element attribute=""abc123='"" attribute2 ='abc123=""' attribute3= ""abc{0} 123="" />
+            </root>";
+
+            markup = string.Format(markup, "\t");
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual("abc123='", root["element"].Attributes["attribute"]);
+            Assert.AreEqual("abc123=\"", root["element"].Attributes["attribute2"]);
+            Assert.AreEqual("abc\t 123=", root["element"].Attributes["attribute3"]);
+
+            root = new QuasiXmlNode( new QuasiXmlParseSettings() { NormalizeAttributeValueWhitespaces = true });
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual("abc 123=", root["element"].Attributes["attribute3"]);
+
+            markup =
+            @"<root>
+                <element attribute=""attributedata"" attribute2="" attribute3=""attrubutedata3"" />
+            </root>";
+
+            root = new QuasiXmlNode(new QuasiXmlParseSettings() { AbortOnError = false });
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual("attribute3=", root["element"].Attributes["attribute2"]);
+        }
+
+        [TestMethod]
+        public void TestCanParseSelfClosingTag()
+        {
+            string markup =
+            @"<root>
+                <element attribute=""attributedata"" attribute2=""attributedata2"" />
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.IsInstanceOfType(root.Children.Single(e => e.Name == "element"), new QuasiXmlNode().GetType());
+
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual("element", root.Children[0].Name);
+            Assert.AreEqual(QuasiXmlNodeType.Element, root["element"].NodeType);
+            Assert.AreEqual(2, root["element"].Attributes.Count);
+            Assert.AreEqual("attributedata", root["element"].Attributes["attribute"]);
+            Assert.AreEqual("attributedata2", root["element"].Attributes["attribute2"]);
+        }
+
+        [TestMethod]
+        public void TestCanParseSelfClosingTagWichIsAlsoRoot()
+        {
+            string markup =
+            @"<root attribute=""attributedata"" attribute2=""attributedata2"" />";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual(0, root.Children.Count);
+            Assert.AreEqual(QuasiXmlNodeType.Element, root.NodeType);
+            Assert.AreEqual(2, root.Attributes.Count);
+            Assert.AreEqual("attributedata", root.Attributes["attribute"]);
+            Assert.AreEqual("attributedata2", root.Attributes["attribute2"]);
+        }
+
+        [TestMethod]
+        public void TestCanParseMarkupWithComment()
+        {
+            string markup =
+            @"<root>
+                <element attribute=""attributedata"" attribute2=""attributedata2"">
+                    <!-- <commentelement attribute=""attributedata"" />commented out text -->live text
+                    <subelement attribute=""attributedata"" />more live text
+                </element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual(4, root["element"].Children.Count);
+            Assert.AreEqual(QuasiXmlNodeType.Comment, root["element"].Children[0].NodeType);
+            Assert.AreEqual(@" <commentelement attribute=""attributedata"" />commented out text ", root["element"].Children[0].Value);
+        }
+
+        [TestMethod]
+        public void TestCanParseMarkupWithCDATA()
+        {
+            string markup =
+            @"<root>
+                <element attribute=""elementattribute"" attribute2=""elementattribute2"">
+                    <![CDATA[cdata content </>""']]>text
+                    <subelement attribute=""elementattribute"" />
+                </element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.IsInstanceOfType(root.Children.Single(e => e.Name == "element"), new QuasiXmlNode().GetType());
+            Assert.AreEqual(3, root["element"].Children.Count);
+            Assert.AreEqual(QuasiXmlNodeType.CDATA, root["element"].Children[0].NodeType);
+            Assert.AreEqual(QuasiXmlNodeType.Text, root["element"].Children[1].NodeType);
+            Assert.AreEqual(QuasiXmlNodeType.Element, root["element"].Children[2].NodeType);
+            Assert.AreEqual(@"cdata content </>""'", root["element"].Children[0].Value);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuasiXmlException), "Missing end tag.")]
+        public void TestShouldThrowExeptionMissingEndTag()
+        {
+            string markup =
+            @"<root>
+                <element attribute=""attributedata"" attribute2=""attributedata2"">
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = true;
+            root.OuterMarkup = markup;
+        }
+
+        [TestMethod]
+        public void TestCanRecoverFromExeptionMissingEndTag()
+        {
+            string markup =
+            @"<root>
+                <element attribute=""attributedata"" attribute2=""attributedata2"">
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = false;
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual(QuasiXmlNodeType.Element, root.NodeType);
+            Assert.AreEqual(0, root.Children.Count);
+        }
+        
+        [TestMethod]
+        [ExpectedException(typeof(QuasiXmlException))]
+        public void TestShouldThrowExeptionMissingOpenTagToClose()
+        {
+            string markup =
+            @"<root><one><two></two></two></one></root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = true;
+            root.OuterMarkup = markup;
+        }
+
+        [TestMethod]
+        public void TestCanRecoverFromExeptionMissingOpenTagToClose()
+        {
+            string markup =
+            @"<root><one><two></two></two></one></root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = false;
+            root.OuterMarkup = markup;
+
+            Assert.IsInstanceOfType(root, typeof(QuasiXmlNode));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuasiXmlException))]
+        public void TestShouldThrowExeptionMissingEndToken()
+        {
+            string markup =
+            @"<root><one></one</root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = true;
+            root.OuterMarkup = markup;
+        }
+
+        [TestMethod]
+        public void TestCanRecoverFromExeptionMissingEndToken()
+        {
+            string markup =
+            @"<root><one></one</root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = false;
+            root.OuterMarkup = markup;
+
+            Assert.IsInstanceOfType(root, typeof(QuasiXmlNode));
+            Assert.AreEqual(0, root.Children.Count);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuasiXmlException))]
+        public void TestShouldThrowExeptionMissingCDATAEndToken()
+        {
+            string markup =
+            @"<root><one><![CDATA[cdata content]]</one></root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = true;
+            root.OuterMarkup = markup;
+        }
+
+        [TestMethod]
+        public void TestCanRecoverFromExeptionMissingCDATAEndToken()
+        {
+            string markup =
+           @"<root><one><![CDATA[cdata content]]</one></root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = false;
+            root.OuterMarkup = markup;
+
+            Assert.IsInstanceOfType(root, typeof(QuasiXmlNode));
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual(1, root.Children[0].Children.Count);
+            Assert.AreEqual(QuasiXmlNodeType.Text, root.Children[0].Children[0].NodeType);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuasiXmlException))]
+        public void TestShouldThrowExeptionMissingCommentEndToken()
+        {
+            string markup =
+            @"<root><one><!-- this is a comment --</one></root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = true;
+            root.OuterMarkup = markup;
+        }
+
+        [TestMethod]
+        public void TestCanRecoverFromExeptionMissingCommentEndToken()
+        {
+            string markup =
+            @"<root><one><!-- this is a comment --</one></root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.ParseSettings.AbortOnError = false;
+            root.OuterMarkup = markup;
+
+            Assert.IsInstanceOfType(root, typeof(QuasiXmlNode));
+            Assert.AreEqual(1, root.Children.Count);
+            Assert.AreEqual(1, root.Children[0].Children.Count);
+            Assert.AreEqual(QuasiXmlNodeType.Text, root.Children[0].Children[0].NodeType);
+        }
+    }
+}
---
+++ 0.9.2b/QuasiXmlTest/QuasiXmlTests.cs
@@ -0,0 +1,131 @@
+﻿/*
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS HEADER.
+ *
+ * Copyright © 2015 Kevin Thomasson, thomassonkevin@gmail.com
+ *
+ * This file is part of QuasiXml.
+ *
+ * QuasiXml is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QuasiXml is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QuasiXml.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * License: GNU Lesser General Public License (LGPL)
+ * Source code: https://quasixml.codeplex.com/
+ */
+using System;
+using System.Xml;
+using QuasiXml;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace QuasiXmlTest
+{
+    [TestClass]
+    public class QuasiXmlTests
+    {
+        [TestMethod]
+        public void TestCanAccessNodesWithIndexingAccessor()
+        {
+            string markup = @"<root>
+                <element>
+                    <subelement attribute=""attributedata"" />
+                </element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            Assert.AreEqual(root["element"]["subelement"].Attributes["attribute"], "attributedata");
+            Assert.AreEqual(root["element"]["subelement"]["nonexisting"], null);
+        }
+
+        [TestMethod]
+        public void TestCanSetParentPropertyCorrectlyWhenModifyingTree()
+        {
+            string markup = @"<root>
+                <element attribute=""attributedata"" attribute2=""attributedata2"">
+                    <subelement attribute=""attributedata"" />
+                </element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            QuasiXmlNode newNode = new QuasiXmlNode();
+            newNode.OuterMarkup = @"<newnode >text</newnode>";
+            root.Children.Add(newNode);
+
+            Assert.AreEqual(root["element"], root["element"]["subelement"].Parent);
+            Assert.AreEqual(root["element"].Parent, root["newnode"].Parent);
+        }
+
+        [TestMethod]
+        public void TestCanSetInnerTextProperty()
+        {
+            string markup = @"<root>
+                <element>This is a test
+                    <subelement>text.</subelement>
+                </element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            root["element"].InnerText = "Hakkuna \t\nmatata";
+
+            Assert.AreEqual("Hakkuna \t\nmatata", root["element"].InnerText);
+            Assert.AreEqual(1, root["element"].Children.Count);
+        }
+
+        [TestMethod]
+        public void TestSystemXmlCharacterEntities()
+        {
+            string markup = @"<root>
+                <element attribute=""data"">This is a text ... and I want to replace the string ""..."" with the XML character entity &amp;ellipsis; post parsing.</element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+            root["element"].Children[0].Value = root["element"].Children[0].Value.Replace("...", "&hellip;");
+
+            XmlDocument document = new XmlDocument();
+            document.LoadXml(markup);
+            document["root"]["element"].ChildNodes[0].Value = document["root"]["element"].ChildNodes[0].Value.Replace("...", "&hellip;");
+
+            Assert.AreNotEqual(document.InnerXml, root.OuterMarkup);
+        }
+
+        [TestMethod]
+        public void TestCanGetDescendants()
+        {
+            string markup =
+            @"<root>
+                <element test=""true"">
+                    <subelement>
+                        <subsubelement test=""true"">text</subsubelement>
+                    </subelement>
+                </element>
+            </root>";
+
+            QuasiXmlNode root = new QuasiXmlNode();
+            root.OuterMarkup = markup;
+
+            var testNodes = root.Descendants.Where(node => node.Attributes.ContainsKey("test") && node.Attributes["test"] == "true").ToList();
+
+            Assert.IsTrue(root.Descendants.Count(node => node.Name == "element") == 1);
+            Assert.IsTrue(root.Descendants.Count(node => node.Name == "subelement") == 1);
+            Assert.IsTrue(root.Descendants.Count(node => node.Name == "subsubelement") == 1);
+            Assert.AreEqual("element", testNodes[0].Name);
+            Assert.AreEqual("subsubelement", testNodes[1].Name);
+        }
+    }
+}
